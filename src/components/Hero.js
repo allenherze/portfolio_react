9b489516ae2999ab52c720609afb81e95e77c1e7
allenherze <joshuac6378@gmail.com>
2021-09-01 10:01:33 +0530

    Finished CSS for mobile and tablet devices

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { ArrowCircleDownIcon } from 'react-line-awesome';
+import { ReactComponent as HeroImage } from '../assets/illustration.svg';
 
 const Hero = () => {
   return (
     <div className='container hero'>
-      <h1 className='header'>Hello There</h1>
-      <div className='content'>
-        <span>I Design and</span>
-        <span>Develop Websites</span>
-        <span>Focusing on </span>
-        <span>Responsive UX / UI</span>
+      <div className='hero-content'>
+        <h1 className='header'>Hello There</h1>
+        <div className='content'>
+          <span>I Design and</span>
+          <span>Develop Websites</span>
+          <span>Focusing on </span>
+          <span>Responsive UX / UI</span>
+        </div>
       </div>
-      <ArrowCircleDownIcon className='arrow-down' />
+      <HeroImage className='hero-image' />
     </div>
   );
 };

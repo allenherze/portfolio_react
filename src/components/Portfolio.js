2b330e9e913198aa63172641f8119c22ef1ce3db
allenherze <joshuac6378@gmail.com>
2021-10-09 16:08:33 +0530

    Finished Desktop CSS

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,28 +9,28 @@ import '../App.css';
 const Portfolio = () => {
   const projects = [
     {
-      id: 1,
+      id: 3,
       name: 'Activity Generator',
       description: 'Are you feeling bored, then have fun with this activity generator. Click on the red button to get the activity and do some cool stuff. ',
       image: imageOne,
-      button: ['View Github', 'Download the APK'],
+      button: ['View Github', 'Visit The Site'],
       buttonLink: ['https://github.com/allenherze/random_activity', 'https://facebook.com'],
     },
     {
-      id: 2,
+      id: 4,
       name: 'Random Quote Generator',
       description: 'A website where you can get a random quote from an array and share it to Facebook and Twitter, also it has Dark mode integrated',
       image: imageTwo,
       button: ['View Github', 'Visit The Site'],
-      buttonLink: ['https://google.com'],
+      buttonLink: ['https://github.com/allenherze/random_quote_generator', 'https://serene-knuth-84ef18.netlify.app/'],
     },
     {
-      id: 3,
+      id: 5,
       name: 'Quiz Time',
       description: 'A single page app where you can answer questions for fun. The questions and the answers are being retrieved from the Open Trivia Database API',
       image: imageThree,
-      button: ['View Github', ''],
-      buttonLink: ['https://google.com'],
+      button: ['View Github', 'Visit The Site'],
+      buttonLink: ['https://github.com/allenherze/basic_quiz', 'https://angry-meitner-965739.netlify.app'],
     },
   ];
 

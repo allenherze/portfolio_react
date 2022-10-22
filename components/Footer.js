d094c0bccc66656b2b8048b0b56556bf33dca809
allenherze <joshuac6378@gmail.com>
2022-10-22 23:09:40 +0530

    Updated social media

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='p-4 mx-auto flex justify-center md:items-center w-full object-bottom'>
-      <span>Copyright @2022 Joshua Coutinho</span>
+    <footer className='p-4 mx-auto flex flex-col justify-center md:items-center w-full object-bottom'>
+      <span className='inline-block'>Copyright @2022 Joshua Coutinho</span>
+      <div className='block grid grid-cols-4 gap-7 mt-2 '>
+        <a href='https://www.behance.net/allenherze' target='_blank' rel='noopener noreferrer'>
+          <img src='/social/behance.svg' alt='' />
+        </a>
+        <a href='https://dribbble.com/allenherze' target='_blank' rel='noopener noreferrer'>
+          <img src='/social/dribbble.svg' alt='' />
+        </a>
+        <a href='https://www.linkedin.com/in/joshuacoutinho/' target='_blank' rel='noopener noreferrer'>
+          <img src='/social/linkedin.svg' alt='' />
+        </a>
+        <a href='https://github.com/allenherze' target='_blank' rel='noopener noreferrer'>
+          <img src='/social/github.svg' alt='' />
+        </a>
+      </div>
     </footer>
   );
 };
